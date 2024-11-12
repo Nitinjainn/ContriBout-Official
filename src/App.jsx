9fa23806e6e87b41e9df8413d15ea15cd6697644
Nitin Jain <nitinjain18072005@gmail.com>
2024-11-12 13:26:28 +0530

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -2,17 +2,18 @@ import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import './App.css'
 import React from "react";
 import Home from './Pages/Home';
-import Signup from './Components/Signup';
 import Login from './Components/Login';
+import Signup from "./Components/SignUp";
 
 
 const App = () => {
   return (
     <Router>
       <div>
+      
         <Routes>
           <Route path='/' element = {<Home/>}/>
-          <Route path='/signup' element = {<Signup/>}/>
+          <Route path='/signup' element = { <Signup/>}/>
           <Route path='/Login' element = {<Login/>}/>
         </Routes>
       </div>
