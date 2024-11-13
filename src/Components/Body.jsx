68bb97516f46772a911aa8dc66617ccd33d08f29
Nitin Jain <nitinjain18072005@gmail.com>
2024-11-13 11:12:34 +0530

    changes

--- src/Components/Body.jsx
+++ src/Components/Body.jsx
@@ -20,9 +20,9 @@ const Body = () => {
   return (
     <div className={`relative w-full h-screen flex justify-center items-center px-4 py-10 ${isModalOpen ? 'overflow-hidden' : ''}`}>
       <div
-        className={`w-full max-w-screen-xl h-[85vh] text-center bg-cover bg-center bg-no-repeat shadow-2xl rounded-2xl relative overflow-hidden border border-gray-200 transition-all duration-500 ${isModalOpen ? 'blur-sm' : ''}`}
+        className={`relative w-full max-w-screen-xl h-[95vh] mx-auto my-8 text-center bg-cover bg-center bg-no-repeat shadow-2xl rounded-2xl overflow-hidden border border-gray-200 transition-all duration-500 ${isModalOpen ? 'blur-sm' : ''}`}
         style={{
-          backgroundImage: "url('https://img.freepik.com/premium-photo/silhouette-two-hands-man-woman-reaching-towards-each-other-sky-sunset-giving-helping-hand_158483-1540.jpg')",
+          backgroundImage: "url('https://miro.medium.com/v2/resize:fit:1200/1*ggfwCv5wd2xih-2MtP9qcw.jpeg')",
           backgroundSize: 'cover',
           backgroundPosition: 'center',
         }}
@@ -33,28 +33,25 @@ const Body = () => {
         <div className="absolute top-4 right-4 z-20">
           <a
             onClick={openModal}
-            className="flex items-center gap-2 rounded-full bg-gradient-to-r from-red-500 via-red-600 to-red-700 px-6 py-3 text-sm font-bold text-white shadow-md transition-transform duration-200 transform hover:scale-105 hover:from-red-600 hover:via-red-700 hover:to-red-800  cursor-pointer"
+            className="flex items-center gap-2 rounded-full bg-gradient-to-r from-red-500 via-red-600 to-red-700 px-6 py-3 text-sm font-bold text-white shadow-md transition-transform duration-200 transform hover:scale-105 hover:from-red-600 hover:via-red-700 hover:to-red-800 cursor-pointer"
           >
             <i className="fas fa-exclamation-circle text-white"></i> Need Money Urgently?
           </a>
         </div>
 
         <div className="relative z-10 flex flex-col items-center justify-center h-full px-6">
-          <h1 className="text-5xl font-extrabold tracking-tight text-white sm:text-7xl drop-shadow-md mb-4">
-            Fund <span className="bg-gradient-to-r from-green-400 via-teal-500 to-yellow-400 text-transparent bg-clip-text">
-  Help Others
-</span>
-
+          <h1 className="text-5xl font-extrabold tracking-tight text-white sm:text-5xl drop-shadow-md mb-4">
+            Travel Boldly, <span className="bg-gradient-to-r from-green-400 via-teal-500 to-yellow-400 text-transparent bg-clip-text">Transact Globally</span>
           </h1>
           <p className="text-lg sm:text-xl font-medium text-gray-100 leading-relaxed mb-8 max-w-2xl drop-shadow-sm">
-            Join us in making a difference. Start funding today to support those in need and create a brighter tomorrow.
+          Connect with Travelers Like You! Effortless Payments, Unique Experiences, and Instant Connections—Join, Pay, and Travel with New Friends Anywhere in the world!
           </p>
           <div className="mt-8 flex flex-col sm:flex-row gap-4">
             <a
               href="/StartFundraising"
               className="rounded-full bg-green-500 px-8 py-4 text-lg font-semibold text-black shadow-lg hover:bg-green-700 hover:shadow-xl transition-all cursor-pointer hover:text-slate-100"
             >
-              Start Fundraising
+              Search Community
             </a>
             <a
               href="#"
